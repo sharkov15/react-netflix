3e8f12545125dbd20fc51bc79de18cc5e7d99dc5
Dmitry Sharkov <49304585+sharkov15@users.noreply.github.com>
2023-01-29 00:21:06 +0300

    Add files via upload

--- src/components/elements/Films/oneFilm.jsx
+++ src/components/elements/Films/oneFilm.jsx
@@ -4,6 +4,7 @@ import './style.css'
 const  OneFilm = () => {
     return ( 
         <div className='Blocks'>
+            <button className='button_nav'>Back</button>
             <a>
                 <div className="project">
                     <img src={project01} className="project__img"/>
@@ -16,6 +17,25 @@ const  OneFilm = () => {
                     <h3 className="project__title">Love Dead Robots</h3>
                 </div>
             </a>
+            <a>
+                <div className="project">
+                    <img src={project01} className="project__img"/>
+                    <h3 className="project__title">Love Dead Robots</h3>
+                </div>
+            </a>
+            <a>
+                <div className="project">
+                    <img src={project01} className="project__img"/>
+                    <h3 className="project__title">Love Dead Robots</h3>
+                </div>
+            </a>
+            <a>
+                <div className="project">
+                    <img src={project01} className="project__img"/>
+                    <h3 className="project__title">Love Dead Robots</h3>
+                </div>
+            </a>
+            <button className='button_nav'>Next</button>
             <a>
                 <div className="project">
                     <img src={project01} className="project__img"/>
